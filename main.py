5a16c85dde62a5ed6aed75d9e2e6ced54937a6cf
Kovalchuk <sashakovalchuk1955@gmail.com>
2024-04-13 20:19:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import sys
 from db.sql_main import set_con, create_connection, get_session
 from db.volunteer_table import VolunteerModel
 
+from configparser import ConfigParser
+
 from datetime import datetime
 from Parsing.ParseDopomogaModul import get_events as ge
 from Parsing.platforma_volunteer import get_events
 
 from bot.misc import main
 
+config = ConfigParser()
+config.read("config.ini")
 
-set_con(create_connection("localhost", "root", "123987MySQL_", "codecon"))
+server = config["DB"]["server"]
+user = config["DB"]["user"]
+password = config["DB"]["password"]
+database = config["DB"]["database"]
+
+set_con(create_connection(server, user, password, database))
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
